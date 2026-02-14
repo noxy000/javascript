04488b6381e01fcf5ba59533ca7db4ed18945970
noxy000 <157939178+noxy000@users.noreply.github.com>
2026-02-14 18:05:59 -0300

    up

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,13 +1,21 @@
 function contar() {
-    var inicio = document.getElementById('txtn')
-    var fim = document.getElementById('txtfim')
-    var passo = document.getElementById('txtp')
+    // 1. Pegamos os elementos
+    var resInicio = document.getElementById('txtn')
+    var resFim = document.getElementById('txtfim')
+    var resPasso = document.getElementById('txtp')
     var contdor = document.getElementById('cont')
-    let i = Number(inicio.value)
-    
-    for (i; inicio.value < fim.value; i += passo.value) {
-        contdor.innerText = (inicio.value)
-    }
-}
 
-//aula 14 ex1 5m
+    // 2. Convertemos os VALORES para números de verdade
+    var i = Number(resInicio.value)
+    var f = Number(resFim.value)
+    var p = Number(resPasso.value)
+
+    // Limpa o que estava escrito antes
+    contdor.innerText = ''
+
+    // 3. O loop agora usa as variáveis numéricas
+    for (var c = i; c <= f; c += p) {
+        contdor.innerText += ` ${c} \u{1F449}` // Acumula o valor e põe um emoji
+    }
+    contdor.innerText += ` \u{1F3C1}` // Bandeirinha no final
+}
